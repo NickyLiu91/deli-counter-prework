5eaae7ad5e5a23bac8f7780aa0b841202e42bba9
Nicky Liu <nickyliu91@gmail.com>
2018-08-12 11:39:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,11 @@ katz_deli = []
 def line(katz_deli)
   sentence = "The line is currently: "
   if katz_deli.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
       sentence = sentence + "#{index + 1}. #{name} "
     end
+    puts sentence
   end
-  puts sentence
 end
